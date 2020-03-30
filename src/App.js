64804015501e627b72b3b85a8341754d75b1a493
freyrdanielsson <freyrdanielsson@gmail.com>
2020-03-30 16:32:39 +0200

    setup redux structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <main className='main'>
+                <Helmet defaultTitle='Footlong' titleTemplate='%s - Footlong'></Helmet>
+                <div className='main__content'>
+                    <Switch location={this.props.location}>
+                        {/* Add routes */}
+                    </Switch>
+                </div>
+            </main>
+        )
+    }
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+
+import rootReducer from './reducers'
 import App from './App';
+import './index.scss';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
