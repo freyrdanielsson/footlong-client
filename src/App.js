4e64e085f7dc56faf3c8d48fdbdb1fe8ec100237
arnar44 <32617343+arnar44@users.noreply.github.com>
2020-04-13 18:33:42 +0200

    Login Functionality (#2)
    * Login implemented
    
    * Implement persistent login
    
    * Register new user functionality
    
    * Style format code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Header from './components/header/Header';
+import UserRoute from './components/userRoute/UserRoute';
 
 import Home from './routes/home/Home';
 import Highlights from './routes/highlights/Highlights';
+import Login from './routes/login/Login';
+import Register from './routes/register/Register';
+
+import { fetchUser } from './actions/auth';
 
 import './App.scss';
 
 
-class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Helmet defaultTitle='Footlong' titleTemplate='%s - Footlong' />
-                <Header />
+function App(props) {
+    const { isAuthenticated, dispatch, location } = props;
+    const token = window.localStorage.getItem('token') || null;
 
-                <main className='app'>
-                    <Switch location={this.props.location}>
-                        <Route exact path='/' component={Home} />
-                        <Route exact path='/highlights' component={Highlights} />
-                    </Switch>
-                </main>
-            </Fragment>
+    useEffect(() => {
+        if (token && !isAuthenticated) dispatch(fetchUser(token));
+    }, [dispatch, isAuthenticated, token]);
 
-        )
+    return (
+        <Fragment>
+            <Helmet defaultTitle='Footlong' titleTemplate='%s - Footlong' />
+            <Header isAuth={isAuthenticated} dispatch={dispatch} />
+
+            <main className='app'>
+                <Switch location={location}>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/highlights' component={Highlights} />
+                    <UserRoute path="/login" authenticated={!isAuthenticated} redirect="/profile" component={Login} />
+                    <UserRoute path="/register" authenticated={!isAuthenticated} redirect="/profile" component={Register} />
+                </Switch>
+            </main>
+        </Fragment>
+
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.isAuthenticated,
     }
 }
 
-export default withRouter(App);
+export default withRouter(connect(mapStateToProps)(App));
