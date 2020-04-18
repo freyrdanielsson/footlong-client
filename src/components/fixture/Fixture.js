e0aad5c482011f97c004aa6be0a3d8d32af1593f
freyrdanielsson <freyrdanielsson@gmail.com>
2020-04-18 18:16:10 +0200

    add score for finished games

--- src/components/fixture/Fixture.js
+++ src/components/fixture/Fixture.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './Fixture.scss';
 
 export default function Fixture(props) {
-    const { homeTeam, awayTeam, status } = props.fixture;
-    
+    const { homeTeam, awayTeam, statusShort, goalsHomeTeam, goalsAwayTeam } = props.fixture;
+
     return (
         <div className='fixture'>
             <div className='fixture__home'>
@@ -15,17 +15,22 @@ export default function Fixture(props) {
                 <div className='fixture__logo'>
                     <img className='fixture__home__logo' src={homeTeam.logo} alt={homeTeam.team_name} />
                 </div>
-                <p className='fixture__status'>{status}</p>
+
+                <div className='fixture__status'>
+                    {statusShort === 'FT' && <p>
+                        {`${goalsHomeTeam} - ${goalsAwayTeam}`}
+                    </p>}
+                    <p className='fixture__short-status'>{statusShort}</p>
+                </div>
+
                 <div className='fixture__logo'>
                     <img className='fixture__away__logo' src={awayTeam.logo} alt={awayTeam.team_name} />
                 </div>
             </div>
 
             <div className='fixture__away'>
-
                 <p>{awayTeam.team_name}</p>
             </div>
-
         </div>
     );
 }
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import queryString from 'query-string';
