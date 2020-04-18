052c7b2c1793b306b3fe8bb94e0d43b51865eea3
freyrdanielsson <freyrdanielsson@gmail.com>
2020-04-18 12:04:38 +0200

    add league logo

--- src/components/league/League.js
+++ src/components/league/League.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-import { fixtureAnimation, leagueAnimation} from './animations';
+import { fixtureAnimation, leagueAnimation } from './animations';
 
 import Fixture from '../fixture/Fixture';
 
 import './League.scss';
 
 export default function League(props) {
-    
+
     const { title, fixtures } = props
-    
+    console.log(fixtures);
+
+
     return (
         <motion.div initial='hidden' animate='visible' variants={leagueAnimation} className='league'>
             <div className='league__header'>
                 <h3 className='league__title'>{title}</h3>
+                <div className={`league__logo league__logo--${title.replace(/\s+/g, '')}`} />
             </div>
             <motion.ul variants={fixtureAnimation} className='league__list'>
                 {fixtures.map((fixture, i) => {
