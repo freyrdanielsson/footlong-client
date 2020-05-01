7664c1f7cad8733f0cdbfb1a5489c1bedd4d2546
freyrdanielsson <freyrdanielsson@gmail.com>
2020-05-01 16:19:44 +0200

    Fix z index problem and add minimum style [datePicker]

--- src/components/datePicker/DatePicker.js
+++ src/components/datePicker/DatePicker.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import REACT_DATE_PICKER from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.scss';
 
 export default function DatePicker(props) {
     const [startDate, setStartDate] = useState(new Date())
 
-    const onChange = (date) => {
+    const onChange = (_date) => {
+        const date = _date || new Date();
         setStartDate(date);
 
         // Keep state logic seperate from parent
@@ -17,11 +19,15 @@ export default function DatePicker(props) {
     }
 
     return (
-        <REACT_DATE_PICKER
-            todayButton='TODAY'
-            dateFormat='yyyy-MM-dd'
-            selected={startDate}
-            onChange={onChange}
-        />
+        <div className='date-picker'>
+            <REACT_DATE_PICKER
+                todayButton='TODAY'
+                dateFormat='yyyy-MM-dd'
+                selected={startDate}
+                onSelect={onChange}
+                className='date-picker__input'
+                calendarClassName='date-picker__calendar'
+            />
+        </div>
     );
 }
--- src/components/fixtureDetails/FixtureDetails.js
+++ src/components/fixtureDetails/FixtureDetails.js
@@ -11,9 +11,6 @@ import './FixtureDetails.scss';
 export default function FixtureDetails(props) {
     const { onClose } = props;
     const { fixture, fixture_events, error, isFetching } = props.fixtureProps;
-    console.log('[FIXTURE]', fixture);
-    console.log('[ERROR]', error);
-    console.log('[LOADING', isFetching);
 
     const teamSide = (teamName) => {
         return (teamName === fixture.homeTeam.team_name)
@@ -30,12 +27,16 @@ export default function FixtureDetails(props) {
                 variants={variants}
                 transition={transition}
                 className='fixtureDetails'>
+
                 <div className='fixtureDetails__header'>
                     <button className='fixtureDetails__close' onClick={onClose}>
                         <span>✕</span>
                     </button>
                     <Fixture fixture={fixture} />
                 </div>
+
+                {error && <p>Unable to get events for this fixture</p>}
+
                 {!isFetching && <Events teamSide={teamSide} fixture_events={fixture_events}/>}
             </motion.div>}
         </AnimatePresence>
