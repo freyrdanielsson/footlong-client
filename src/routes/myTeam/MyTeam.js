bb3d00dcfe846416c5eda9177089a518b880e952
arnar44 <arnar44@gmail.com>
2020-05-04 14:59:44 +0200

    Fix helmet on my-team route

--- src/routes/myTeam/MyTeam.js
+++ src/routes/myTeam/MyTeam.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 import './MyTeam.scss';
 
