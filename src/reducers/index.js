60e1d83f729b4ae827c2199152c49695f442e37e
Freyr Saputra Daníelsson <32617790+freyrdanielsson@users.noreply.github.com>
2020-05-08 18:17:41 +0200

    center content (#20)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux'
 import auth from './auth';
 import fixtures from './fixtures';
-import highlights from './highlights';
 import register from './register';
 import players from './players';
 import teams from './teams';
@@ -9,7 +8,6 @@ import team from './team';
 
 export default combineReducers({
     auth,
-    highlights,
     fixtures,
     register,
     players,
